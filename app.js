79825df839e166208cfbecc6993966119493ac1d
Miha <miha.jack@gmail.com>
2017-01-30 19:44:23 +0100

    popucal route

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var modules = require('./routes');
+var routes = require('./routes');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get(modules.getUsers.route, modules.getUsers.callback);
-
-app.get(modules.getUser.route, modules.getUser.callback);
-
-app.post(modules.createUser.route, modules.createUser.callback);
-
-app.put(modules.modifyUser.route, modules.modifyUser.callback);
-
-app.delete(modules.deleteUser.route, modules.deleteUser.callback);
+routes.forEach(function (x) {
+    app[x["method"].toLowerCase()](x.route, x.callback);
+})
 
 
 app.listen(3000);
