e36db30b5c08d36931cd69b0b1fc4cce5dcc6544
Miha <miha.jack@gmail.com>
2017-01-31 15:47:49 +0100

    added article.js

---
+++ article.js
@@ -0,0 +1,32 @@
+module.exports = [
+    { 
+        route: '/article/',
+        callback: function(req, res) {
+
+            res.json(artDb.articles);
+        },
+        method: "get",
+    },
+
+    {
+
+    },
+
+    {
+
+    },
+
+    {
+
+    },
+];
+
+var artDb = { 
+    articles: {
+        articleOne: {
+            author: "John Doe",
+            title: "Origins of leprechaun fairies",
+            ID: 462,
+        },
+    },
+};
